0cf40c6b6849971f9e2e4e0180752e6efab3c65a
Raiyhan Bhuiyan <96577789+raiyhanbhuiyan17@users.noreply.github.com>
2023-02-25 11:48:47 -0500

    second commit main file

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+ a = int(input("Enter the first number: "))
+ b = int(input("Enter the second number: "))
+ print(a + b)
+ # s = a+b
+ # print(s)
+
+ print(a - b)
+ # d = a-b
+ # print(d)
+
+ print(a * b)
+ # p = a*b
+ # print(p)
+
+#
